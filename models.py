3d97fd9534b27a04640aea467f7b3bcc9c6e6c2b
Isla Nova Networks <hi.islanova@gmail.com>
2025-07-17 12:04:03 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,51 +1,88 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-from sqlalchemy import create_engine
-import os
+from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def create_db_and_tables():
-    Base.metadata.create_all(bind=engine)
 
 class Compania(Base):
     __tablename__ = "companias"
     id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, unique=True, index=True)
+    nombre = Column(String, index=True)
 
 class Usuario(Base):
     __tablename__ = "usuarios"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    password = Column(String)
     compania_id = Column(Integer, ForeignKey("companias.id"))
     compania = relationship("Compania")
 
 class Cliente(Base):
     __tablename__ = "clientes"
     id = Column(Integer, primary_key=True, index=True)
-    zona = Column(Integer)
-    entrada = Column(Integer)
+    nombre = Column(String)
+    telefono = Column(String)
+    zona = Column(String)  # Changed to String to match Excel's named zones
+    subzonas = Column(String)
+    precio = Column(Float)
+    tipo_vivienda = Column(String)
+    finalidad = Column(String)
     habitaciones = Column(Integer)
-    banos = Column(Integer)
+    banos = Column(String)  # e.g., "1+1" from Excel
+    estado = Column(String)
+    ascensor = Column(String)
+    bajos = Column(String)
+    entreplanta = Column(String)
+    m2 = Column(Integer)
+    altura = Column(String)
+    cercania_metro = Column(String)
+    orientacion = Column(String)
+    edificio_semi_nuevo = Column(String)
+    adaptado_movilidad = Column(String)
+    balcon = Column(String)
+    patio = Column(String)
+    terraza = Column(String)
+    garaje = Column(String)
+    trastero = Column(String)
+    interior = Column(String)
+    piscina = Column(String)
+    urbanizacion = Column(String)
+    vistas = Column(String)
+    caracteristicas_adicionales = Column(String)
+    banco = Column(String)
+    ahorro = Column(Float)
     compania_id = Column(Integer, ForeignKey("companias.id"))
+    compania = relationship("Compania")
 
 class Piso(Base):
     __tablename__ = "pisos"
     id = Column(Integer, primary_key=True, index=True)
-    zona = Column(Integer)
-    precio = Column(Integer)
+    precio = Column(Float)
+    tipo_vivienda = Column(String)
     habitaciones = Column(Integer)
-    banos = Column(Integer)
+    banos = Column(String)
+    estado = Column(String)
+    ascensor = Column(String)
+    bajos = Column(String)
+    entreplanta = Column(String)
+    m2 = Column(Integer)
+    altura = Column(String)
+    cercania_metro = Column(String)
+    orientacion = Column(String)
+    edificio_semi_nuevo = Column(String)
+    adaptado_movilidad = Column(String)
+    balcon = Column(String)
+    patio = Column(String)
+    terraza = Column(String)
+    garaje = Column(String)
+    trastero = Column(String)
+    interior = Column(String)
+    piscina = Column(String)
+    urbanizacion = Column(String)
+    vistas = Column(String)
+    caracteristicas_adicionales = Column(String)
     compania_id = Column(Integer, ForeignKey("companias.id"))
+    compania = relationship("Compania")
+
+def create_db_and_tables():
+    Base.metadata.create_all(bind=engine)
